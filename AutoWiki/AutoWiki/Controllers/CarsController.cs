8bd78f6f7e96234a3c6df0a9bd2886c001c99cbd
Todor <Todor@DESKTOP-UMBQRMA>
2016-05-25 15:14:29 +0300

    class + controller

---
+++ AutoWiki/AutoWiki/Controllers/CarsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoWiki.Models;
+
+namespace AutoWiki.Controllers
+{
+    public class CarsController : Controller
+    {
+        private CarsDBContent db = new CarsDBContent();
+
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View(db.Car.ToList());
+        }
+
+        // GET: Cars/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cars cars = db.Car.Find(id);
+            if (cars == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cars);
+        }
+
+        // GET: Cars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cars/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Brand,Model,Engine,HorsePower,Coupe,MaxSpeed")] Cars cars)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Car.Add(cars);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cars);
+        }
+
+        // GET: Cars/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cars cars = db.Car.Find(id);
+            if (cars == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cars);
+        }
+
+        // POST: Cars/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Brand,Model,Engine,HorsePower,Coupe,MaxSpeed")] Cars cars)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cars).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cars);
+        }
+
+        // GET: Cars/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cars cars = db.Car.Find(id);
+            if (cars == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cars);
+        }
+
+        // POST: Cars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cars cars = db.Car.Find(id);
+            db.Car.Remove(cars);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AutoWiki/AutoWiki/Models/Cars.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AutoWiki.Models
+{
+    public class Cars
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Engine { get; set; }
+        public string HorsePower { get; set; }
+        public string Coupe { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+    public class CarsDBContent : DbContext
+    {
+        public DbSet<Cars> Car { get; set; }
+    }
+}
