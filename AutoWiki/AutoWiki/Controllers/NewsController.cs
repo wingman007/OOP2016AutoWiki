ab070030e2734a656aded4b930de7ce06d9c9e07
Todor <Todor@DESKTOP-UMBQRMA>
2016-06-06 00:24:54 +0300

    Picture test

---
+++ AutoWiki/AutoWiki/Controllers/NewsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoWiki.Controllers
+{
+   
+        public class NewsController : Controller
+        {
+            // GET: News
+            public ActionResult Index()
+            {
+                return View();
+                //  return "here is the news";
+            }
+
+            public ActionResult News(string name, int numTimes = 0)
+            {
+                ViewBag.name = name;
+                ViewBag.numTimes = numTimes;
+                return View();
+                // return "Audi";
+            }
+        }
+    }
