acdee3e5c6b37fe45637c3f707dc42fd15c91011
Viktoriya Petrova <viki_petrova9@abv.bg>
2016-06-05 19:43:53 +0300

    Created News and About

---
+++ AutoWiki/AutoWiki/Models/News.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AutoWiki.Models
+{
+    public class News
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string About { get; set; }
+
+    }
+    public class NewsDBContent : DbContext
+    {
+        public DbSet<News> New { get; set; }
+    }
+}
